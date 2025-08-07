374e23babd2310ad6c9da27694ae73316a5b3057
Soham Bambade <bambadesoham408@gmail.com>
2025-08-07 10:44:54 +0000

    github api data

---
+++ src/app/functions.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import React, { use, useEffect } from 'react'
+
+function functions() {
+
+    useEffect(() => {
+        console.log('hello my name soham');
+        
+    }, [])
+
+  return (
+    <div>
+        <>
+        </>
+    </div>
+  )
+}
+
+export default functions
